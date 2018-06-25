c4f035293ae1d49701220643e8142fd10f9706d6
Ryan Zlock <rwzlock@gmail.com>
2018-06-24 22:19:56 -0700

    Completed Part 1.

--- logic.js
+++ logic.js
@@ -1,32 +1,119 @@
 // Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_hour.geojson";
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson";
+
+var myMap = L.map("map", {
+  center: [37.09, -95.71],
+  zoom: 5
+});
+
+L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+  "access_token=pk.eyJ1Ijoicnd6bG9jayIsImEiOiJjamlkdmVuaWYwZnZjM2twZWs3eWY4MTdyIn0." +
+  "PZhghkJRnVia5SkmmtnuyA").addTo(myMap);
+
+
+function markerSize(magnitude) {
+  return magnitude*magnitude*magnitude * 5000;
+}
+function markerColor(magnitude) {
+    if (2.5 <= magnitude && magnitude <= 2.9) {return "yellow"}
+
+    else if (2.9 < magnitude && magnitude <=3.3) {return "orange"}  
+
+    else if (3.3 < magnitude && magnitude <=3.7) {return "red"}
+        
+    else if (3.7 < magnitude && magnitude <=4.1) {return "green"}
+        
+    else if (4.1 < magnitude && magnitude <=4.5) {return "blue"}
+        
+    else if (4.5 < magnitude && magnitude <=5) {return "purple"}
+        
+    else if (5 < magnitude && magnitude <=5.5) {return "navy"}
+        
+    else if (5.5 < magnitude && magnitude <=6) {return "brown"}
+        
+    else if (magnitude > 6) {return "black"}
+        
+    else {return "white"}      
+}
 
 // Perform a GET request to the query URL
 d3.json(queryUrl, function(data) {
-  // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
+  var earthquakeData = data.features;
+  createEarthquakes(earthquakeData)
 });
 
-function createFeatures(earthquakeData) {
+function createEarthquakes(earthquakeData) {
 
-  // Define a function we want to run once for each feature in the features array
-  // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><br><h3>" + feature.properties.mag +"<hr><p>" + new Date(feature.properties.time) + "</p>");
+  var earthquakes = []
+
+  for (var i = 0; i < earthquakeData.length-1; i++) {
+    var earthquake_dict = 
+    {
+      name: earthquakeData[i].properties.place,
+      location: [ earthquakeData[i].geometry.coordinates[1], earthquakeData[i].geometry.coordinates[0] ],
+      magnitude: earthquakeData[i].properties.mag
+    }
+    earthquakes.push(earthquake_dict)
   }
-
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
+  console.log(earthquakes)
+  console.log(earthquakes[0].magnitude)
   // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
+  //createMap(earthquakeMarkers);
+  createFeatures(earthquakes)
 }
 
-function createMap(earthquakes) {
+function createFeatures(earthquakes) {
+  for (var j = 0; j < earthquakes.length; j++) {
+    console.log(earthquakes[j].magnitude)
+    L.circle(earthquakes[j].location, {
+      fillOpacity: 0.75,
+      color: markerColor(earthquakes[j].magnitude),
+      fillColor: markerColor(earthquakes[j].magnitude),
+      // Setting our circle's radius equal to the output of our markerSize function
+      // This will make our marker's size proportionate to its population
+      radius: markerSize(earthquakes[j].magnitude)
+    }).bindPopup("<h1>" + earthquakes[j].name + "</h1> <hr> <h3>Magnitude: " + earthquakes[j].magnitude + "</h3>").addTo(myMap);
+  }
+}
+
+
+/*
+
+for (var i = 0; i < earthquakeData.length; i++) {
+  //console.log(earthquakeData[i].geometry.coordinates[0])
+  // Setting the marker radius for the state by passing population into the markerSize function
+  earthquakeMarkers.push(
+    L.circle([earthquakeData[i].geometry.coordinates[0], earthquakeData[i].geometry.coordinates[1]], {
+      stroke: false,
+      fillOpacity: 0.75,
+      color: "white",
+      fillColor: "white",
+      radius: markerSize(earthquakeData[i].properties.mag)
+    })
+  );
+  L.bindPopup("<h3>" + earthquakeData[i].properties.place +
+    "</h3><br><h3>" + earthquakeData[i].properties.mag +"<hr><p>" + new Date(earthquakeData[i].properties.time) + "</p>");
+}
+
+
+earthquakeMarkers.push(
+  L.circle([earthquakeData[i].geometry.coordinates[0], earthquakeData[i].geometry.coordinates[1]], {
+    stroke: false,
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: "white",
+    radius: markerSize(earthquakeData[i].properties.mag)
+  })
+);
+L.bindPopup("<h3>" + earthquakeData[i].properties.place +
+  "</h3><br><h3>" + earthquakeData[i].properties.mag +"<hr><p>" + new Date(earthquakeData[i].properties.time) + "</p>");
+
+
+
+
+
+
+function createMap(earthquakeMarkers) {
 
   // Define streetmap and darkmap layers
   var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
@@ -42,10 +129,12 @@ function createMap(earthquakes) {
     "Street Map": streetmap,
     "Dark Map": darkmap
   };
+  var earthquakes = L.layerGroup(earthquakeMarkers);
 
   // Create overlay object to hold our overlay layer
   var overlayMaps = {
-    Earthquakes: earthquakes
+    //Earthquakes: earthquakes,
+    "Magnitudes": earthquakes
   };
 
   // Create our map, giving it the streetmap and earthquakes layers to display on load
@@ -54,7 +143,7 @@ function createMap(earthquakes) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [streetmap, earthquakes]
+    layers: [streetmap, earthquakes1]
   });
 
   // Create a layer control
@@ -64,3 +153,5 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
+
+*/
